0f43327a714d9ede627b1b9ebfe5f05278c5bb2e
Damil3 <33283731+Damil3@users.noreply.github.com>
2017-11-09 13:53:14 +0100

    Update RaceTurtle.scala

--- src/main/scala/turtlerace/RaceTurtle.scala
+++ src/main/scala/turtlerace/RaceTurtle.scala
@@ -4,6 +4,7 @@ import graphics.Point
 import cslib.window.SimpleWindow
 import java.awt.Color
 
+
 class RaceTurtle(
   override val window: RaceWindow,
   var nbr: Int,
@@ -18,10 +19,11 @@ class RaceTurtle(
   val defaultStep = 6
 
   /** Tar ett steg av slumpmässig längd i Range(0,defaultStep) */
-  def raceStep(): Unit = ???
+  def raceStep(): Unit = forward(scala.util.Random.nextInt(defaultStep))
+
 
   /** Återgår till initPosition */
-  def restart(): Unit  = ???
+  def restart(): Unit  = home
 
   override def toString: String = s"#$nbr name: $name"
 }
